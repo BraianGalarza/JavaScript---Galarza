06f597514e725172a949a08be3c174b635481668
BraianGalarza <galarzabraian01@gmail.com>
2022-06-06 21:35:25 -0300

    Simulador Interactivo

--- app.js
+++ app.js
@@ -1,75 +1,99 @@
+//SUMA TOTAL DE PRODUCTOS QUE QUIERA EL USUARIO EN UNA COMPRA MAYORISTA
+console.log("Carrito de compras")
 
-console.log("Suma - Resta - Multiplicación - División")
-
+//Declaro variables
 let fin = 1
-
+let total = 0
+let PrimerProducto = 1
+let ConcatProduct = ""
+let ConcatProduct2 = ""
+let producto = ""
+let valor = 0
+let cantidad = 0
 while (fin == 1){
 
-    let resultado = 0
-    let comprob = 1
-    let Num = +prompt("Ingrese un Número")
-    let Operacion = prompt("Ingrese una Operación (+ - * /)")
-    let Num2 = +prompt("Ingrese un Segundo Número")
+    // Usuario ingresa el nombre del producto, ej: "Camisa"
+    producto = prompt("Ingrese nombre del producto")
+    while (producto == "" || producto == undefined){
 
-    if (Operacion ==  "+") {
-
-        resultado = Num + Num2
-        comprob = 2
+        producto = prompt("Ingrese nombre del producto")
 
     }
-    else if(Operacion ==  "-") {
-
-        resultado = Num - Num2
-        comprob = 2
-
+    // Usuario ingresa el valor de cada producto, ej: 10
+    valor = +prompt("Ingrese el valor del producto")
+    // Comprueba que el valor ingresado sea un numero, si no es un numero deja la variable vacia
+    if (isNaN(valor)){
+        console.log("Por favor ingrese numeros validos. Vuelva a intentar.")
+        valor = ""
     }
-    else if(Operacion ==  "/") {
+    // Bucle para que ingrese un numero valido si es que se ingreso un caracter invalido
+    while (valor == "" || valor == undefined){
 
-        resultado = Num / Num2
-        comprob = 2
-
-    }
-    else if(Operacion ==  "*") {
-
-        resultado = Num * Num2
-        comprob = 2
-
-    }
-
-
-    if (comprob == 2) {
-
-        if (isNaN(resultado)){
+        valor = +prompt("Ingrese un valor de cada producto")
+        if (isNaN(valor)){
             console.log("Por favor ingrese numeros validos. Vuelva a intentar.")
+            valor = ""
         }
-        else {
 
-            console.log("El resultado es: " + resultado)
+    }
+    // Usuario ingresa la cantidad de unidades de ese mismo producto, ej: 3
+    cantidad = +prompt("Ingrese la cantidad de unidades del producto")
+    // Comprueba que el valor ingresado sea un numero, si no es un numero deja la variable vacia
+    if (isNaN(cantidad)){
 
-            while (true){
-    
-                let consulta = prompt("Desea hacer alguna otra operacion. (S/N)")
-                if (consulta == "N" || consulta == "n" ){
-                    fin = 2
-                    break
-                }
-                else if (!(consulta == "S") && !(consulta == "s")){
-                    
-                    console.log("Por favor ingerse un caracter valido. (S/N)")
-                
-                }
-                else{
-                    break
-                }
+        console.log("Por favor ingrese numeros validos. Vuelva a intentar.")
+        cantidad = ""
+    }
+    // Bucle para que ingrese un numero valido si es que se ingreso un caracter invalido
+    while (cantidad == "" || cantidad == undefined){
         
-            }
+        cantidad = +prompt("Ingrese la cantidad de unidades del producto")
+        if (isNaN(cantidad)){
+
+            console.log("Por favor ingrese numeros validos. Vuelva a intentar.")
+            cantidad = ""
         }
     }
-    else {
-        console.log("Por favor ingrese una operación valida. Vuelva a intentar.")
+    // Print de los valores ingresados para llevar un registro visual de lo que ingrese el usuario
+    console.log("Producto ingresado: " + producto)
+    console.log("Valor de cada " + producto + " : " + valor + "$")
+    console.log("Cantidad ingresada: " + cantidad)
+
+    // Condicional utilizado para ver el total de los productos segun corresponda
+    if (PrimerProducto == 1){
+
+        total = (cantidad * valor)
+        console.log("El monto total sumando el producto: " + producto + ", es de: " + total + "$")
+        ConcatProduct = producto
+    }
+    else{
+
+        total = total + (cantidad * valor)
+        ConcatProduct2 = ConcatProduct + " y " + producto
+        ConcatProduct = ConcatProduct + ", " + producto
+        console.log("El monto total sumando los productos: " + ConcatProduct2 + ". Es de: " + total + "$")
+
     }
 
+    // Bucle para preguntarle al usuario si desea seguir ingresando productos
+    while (true){
 
+        let consulta = prompt("Desea sumar algun otro producto. (S/N)")
+        if (consulta == "N" || consulta == "n" ){
 
+            fin = 2
+            break
+        }
+        else if (!(consulta == "S") && !(consulta == "s")){
+                
+             console.log("Por favor ingerse un caracter valido. (S/N)")
+            
+        }
+        else{
 
+            PrimerProducto = 2
+            break
+        }
+    
+    }
 }
