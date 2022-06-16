d026a232fecd769f8227916415dc6658ebc0e250
BraianGalarza <galarzabraian01@gmail.com>
2022-06-15 21:41:04 -0300

    Primer entrega del proyecto final

--- app.js
+++ app.js
@@ -1,23 +1,118 @@
 //SUMA TOTAL DE PRODUCTOS QUE QUIERA EL USUARIO EN UNA COMPRA MAYORISTA
 console.log("Carrito de compras")
 
-//Declaro variables
-let fin = 1
+//Variables
+const arrayObjetos = []
+let menuOpcion = 0
 let total = 0
-let PrimerProducto = 1
-let ConcatProduct = ""
-let ConcatProduct2 = ""
-let producto = ""
-let valor = 0
-let cantidad = 0
-let arrayObjetos = []
-while (fin == 1) {
+do{
+    //If para mostrar el menu si hay datos en el array
+    if (arrayObjetos.length == 0){
+        console.log("Menú de opciones")
+        console.log("1. Agregar producto")
+        console.log("0. Salir")
+        menuOpcion = +prompt("Ingrese un numero")
+        if (menuOpcion == 1){
 
+            ingreso_Objeto_A_Array()       
+
+        }
+    }else{
+        //Menu de opciones para cuando el array tenga datos
+        console.log("Menú de opciones")
+        console.log("1. Agregar producto")
+        console.log("2. Modificar producto")
+        console.log("3. Eliminar producto")
+        console.log("4. Lista de producto/s")
+        console.log("0. Salir")
+        menuOpcion = +prompt("Ingrese un numero")
+        //Switch utilizado para realizar la accion que ingreso el usuario
+        switch(menuOpcion){
+            case 1:
+                ingreso_Objeto_A_Array()
+                break
+            case 2:
+                let modificacionOpcion = 0
+                console.log("Cual producto desea modificar?")
+                for(const producto of arrayObjetos){
+                    console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
+                }
+                let idM = +prompt("Ingrese numero del producto")
+                console.log("Opciones a modificar")
+                console.log("1. Nombre del producto")
+                console.log("2. Valor del producto")
+                console.log("3. Cantidad del producto")
+                console.log("0. Salir")
+                modificacionOpcion = +prompt("Ingrese un numero según la opción a modificar")
+                let indexM = arrayObjetos.findIndex((producto) => producto.idProducto == idM)
+                switch(modificacionOpcion){
+                    case 1:
+                        if (indexM != -1){
+                            arrayObjetos[indexM].nombre = prompt("Ingrese el nuevo nombre del producto.")
+                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                            break
+                        } else {
+                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
+                        }
+                    case 2:
+                        if (indexM != -1){
+                            arrayObjetos[indexM].valor = prompt("Ingrese el nuevo valor del producto.")
+                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                            break
+                        } else {
+                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
+                        }
+                    case 3:
+                        if (indexM != -1){
+                            arrayObjetos[indexM].cantidad = prompt("Ingrese la nueva cantidad del producto.")
+                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                            break
+                        } else {
+                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
+                        }
+                }
+                break
+            case 3:
+                console.log("Cual producto desea eliminar?")
+                for(const producto of arrayObjetos){
+                    console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
+                }
+                let idE = +prompt("Ingrese numero del producto")
+                let indexE = arrayObjetos.findIndex((producto) => producto.idProducto == idE)
+                if (indexE != -1){
+                    let eliminado = arrayObjetos.splice(indexE,1)
+                    console.log("El producto: " + eliminado[0].nombre + ", fue eliminado con exito")
+                    arrayObjetos.forEach((producto, indice) => {
+                        producto.idProducto = indice + 1
+                    })
+                    break
+                } else {
+                    console.log("Nombre de producto invalido, por favor vuelva a intentar.")
+                }
+                break
+            case 4:
+                total = 0
+                arrayObjetos.forEach((producto, indice) => {
+                    total = total + ((producto.cantidad) * (producto.valor))
+                    ConcatProduct = `${indice == 0 ? producto.nombre : ConcatProduct + `${indice == arrayObjetos.length - 1 ? " y " : ", "}` + producto.nombre}`
+                })
+                console.log(`El monto total sumando ${arrayObjetos.length>1?"los productos":"el producto"}: ${ConcatProduct}. Es de: $${total}`)
+                break
+        }
+    }
+}while(menuOpcion!=0)
+
+function ingreso_Objeto_A_Array() {
+    let nombre = ""
+    let valor = 0
+    let cantidad = 0
+    //Id interno del producto
+    let idProducto = arrayObjetos.length + 1
     // Usuario ingresa el nombre del producto, ej: "Camisa"
-    producto = prompt("Ingrese nombre del producto")
-    while (producto == "" || producto == undefined) {
+    nombre = prompt("Ingrese nombre del producto")
+    while (nombre == "" || nombre == undefined) {
 
-        producto = prompt("Ingrese nombre del producto")
+        nombre = prompt("Ingrese nombre del producto")
 
     }
     // Usuario ingresa el valor de cada producto, ej: 10
@@ -40,44 +135,17 @@ while (fin == 1) {
 
     }
     // Print de los valores ingresados para llevar un registro visual de lo que ingrese el usuario
-    console.log("Producto ingresado: " + producto)
-    console.log("Valor de cada " + producto + " : " + valor + "$")
+    console.log("Producto ingresado: " + nombre)
+    console.log("Valor de cada " + nombre + " : " + valor + "$")
     console.log("Cantidad ingresada: " + cantidad)
     // Declaro el objeto "compra", en donde se guardan los datos anteriormente ingresados
-    const compra = {
-        producto: producto,
+    let objeto = {
+        nombre: nombre,
         valor: valor,
-        cantidad: cantidad
+        cantidad: cantidad,
+        idProducto: idProducto
     }
     //Push al array cada compra realizada
-    arrayObjetos.push(compra)
-    // ForEach utilizado para ver el total de los productos segun corresponda
-    arrayObjetos.forEach((compra, indice) => {
+    arrayObjetos.push(objeto)            
 
-        total = total + ((compra.cantidad) * (compra.valor))
-        //total = (compra.cantidad * compra.valor) 
-        ConcatProduct = `${indice == 0 ? compra.producto : ConcatProduct + `${indice == arrayObjetos.length - 1 ? " y " : ", "}` + compra.producto}`
-        console.log(`El monto total sumando ${arrayObjetos.length>1?"los productos":"el producto"}: ${ConcatProduct}. Es de: $${total}`)
-    })
-
-
-    // Bucle para preguntarle al usuario si desea seguir ingresando productos
-    while (true) {
-
-        let consulta = prompt("Desea sumar algun otro producto. (S/N)")
-        if (consulta == "N" || consulta == "n") {
-
-            fin = 2
-            break
-        } else if (!(consulta == "S") && !(consulta == "s")) {
-
-            console.log("Por favor ingerse un caracter valido. (S/N)")
-
-        } else {
-
-            PrimerProducto = 2
-            break
-        }
-
-    }
 }
