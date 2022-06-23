722947b3d8648a6b77125ee89c71f82ac1ea456a
BraianGalarza <galarzabraian01@gmail.com>
2022-06-22 23:49:42 -0300

    Eventos

--- app.js
+++ app.js
@@ -1,123 +1,36 @@
 //SUMA TOTAL DE PRODUCTOS QUE QUIERA EL USUARIO EN UNA COMPRA MAYORISTA
 console.log("Carrito de compras")
 
+console.log("Carrito de compras")
+
 //Variables
 const arrayObjetos = []
 let menuOpcion = 0
 let total = 0
-do{
-    //If para mostrar el menu si hay datos en el array
-    if (arrayObjetos.length == 0){
-        console.log("Menú de opciones")
-        console.log("1. Agregar producto")
-        console.log("0. Salir")
-        menuOpcion = +prompt("Ingrese un numero")
-        if (menuOpcion == 1){
 
-            const producto = pedirDatos()
-            ingreso_Objeto_A_Array(producto)       
-            
-        }
-    }else{
-        //Menu de opciones para cuando el array tenga datos
-        console.log("Menú de opciones")
-        console.log("1. Agregar producto")
-        console.log("2. Modificar producto")
-        console.log("3. Eliminar producto")
-        console.log("4. Lista de producto/s")
-        console.log("5. Imprimir lista en pantalla y salir")
-        console.log("0. Salir")
-        menuOpcion = +prompt("Ingrese un numero")
-        //Switch utilizado para realizar la accion que ingreso el usuario
-        switch(menuOpcion){
-            case 1:
-                const producto = pedirDatos()
-                ingreso_Objeto_A_Array(producto)
-                break
-            case 2:
-                let modificacionOpcion = 0
-                console.log("Cual producto desea modificar?")
-                for(const producto of arrayObjetos){
-                    console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
-                }
-                let idM = +prompt("Ingrese numero del producto")
-                console.log("Opciones a modificar")
-                console.log("1. Nombre del producto")
-                console.log("2. Valor del producto")
-                console.log("3. Cantidad del producto")
-                console.log("0. Salir")
-                modificacionOpcion = +prompt("Ingrese un numero según la opción a modificar")
-                let indexM = arrayObjetos.findIndex((producto) => producto.idProducto == idM)
-                switch(modificacionOpcion){
-                    case 1:
-                        if (indexM != -1){
-                            arrayObjetos[indexM].nombre = prompt("Ingrese el nuevo nombre del producto.")
-                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
-                            break
-                        } else {
-                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
-                        }
-                    case 2:
-                        if (indexM != -1){
-                            arrayObjetos[indexM].valor = prompt("Ingrese el nuevo valor del producto.")
-                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
-                            break
-                        } else {
-                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
-                        }
-                    case 3:
-                        if (indexM != -1){
-                            arrayObjetos[indexM].cantidad = prompt("Ingrese la nueva cantidad del producto.")
-                            console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
-                            break
-                        } else {
-                            console.log("Numero de producto invalido, por favor vuelva a intentar.")
-                        }
-                }
-                break
-            case 3:
-                console.log("Cual producto desea eliminar?")
-                for(const producto of arrayObjetos){
-                    console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
-                }
-                let idE = +prompt("Ingrese numero del producto")
-                let indexE = arrayObjetos.findIndex((producto) => producto.idProducto == idE)
-                if (indexE != -1){
-                    let eliminado = arrayObjetos.splice(indexE,1)
-                    console.log("El producto: " + eliminado[0].nombre + ", fue eliminado con exito")
-                    arrayObjetos.forEach((producto, indice) => {
-                        producto.idProducto = indice + 1
-                    })
-                    break
-                } else {
-                    console.log("Nombre de producto invalido, por favor vuelva a intentar.")
-                }
-                break
-            case 4:
-                total = 0
-                arrayObjetos.forEach((producto, indice) => {
-                    total = total + ((producto.cantidad) * (producto.valor))
-                    ConcatProduct = `${indice == 0 ? producto.nombre : ConcatProduct + `${indice == arrayObjetos.length - 1 ? " y " : ", "}` + producto.nombre}`
-                })
-                console.log(`El monto total sumando ${arrayObjetos.length>1?"los productos":"el producto"}: ${ConcatProduct}. Es de: $${total}`)
-                break
-            case 5:
-                //Aplicando DOM para insertar en el body algunos div con lista que contienen cada producto ingresado con sus detalles
-                total = 0
-                for (const producto of arrayObjetos){
-                    total = total + ((producto.cantidad) * (producto.valor))
-                    let contenedor = document.createElement("div")
-                    contenedor.innerHTML = `<ul> ID: ${producto.idProducto}</ul><li> Producto: ${producto.nombre}</li><li> Cantidad: ${producto.cantidad}</li><li>Valor: $${producto.valor}</li>`
-                    document.body.appendChild(contenedor)
-                }
-                let parrafo = document.createElement("p")
-                parrafo.innerHTML = `<h2>Monto total de la compra: $${total}</h2>`
-                document.body.append(parrafo)
-                menuOpcion = 0
-                break
-        }
-    }
-}while(menuOpcion!=0)
+//esconde botones de modificacion al iniciar 
+document.getElementById("btnmodprod").hidden = true
+document.getElementById("btneliminarprod").hidden = true
+
+
+//escucha los botones
+let botonAgregarProd = document.getElementById("btnagregarprod")
+botonAgregarProd.addEventListener("click", clickAgregarProd)
+
+let botonModProd = document.getElementById("btnmodprod")
+botonModProd.addEventListener("click", clickModProd)
+
+let botonEliminaProd = document.getElementById("btneliminarprod")
+botonEliminaProd.addEventListener("click", clickEliminarProd)
+
+
+
+
+
+
+
+//Funciones
+
 
 function ingreso_Objeto_A_Array(producto) {
     //Push al array cada compra realizada
@@ -167,4 +80,137 @@ function pedirDatos() {
         cantidad: cantidad,
         idProducto: idProducto
     }
+}
+
+
+
+
+//Funciones (botones)
+
+
+//funcion click agregar producto
+function clickAgregarProd(){
+    const producto = pedirDatos()
+    ingreso_Objeto_A_Array(producto)  
+    hidennButonn()
+    ListaPantalla()
+}
+
+//funcion click modificar producto
+function clickModProd(){
+    let modificacionOpcion = 0
+    console.log("Cual producto desea modificar?")
+    for(const producto of arrayObjetos){
+        console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
+    }
+    let idM = +prompt("Ingrese numero del producto")
+    let indexM = arrayObjetos.findIndex((producto) => producto.idProducto == idM)
+    if (indexM != -1){
+
+        console.log("Opciones a modificar")
+        console.log("1. Nombre del producto")
+        console.log("2. Valor del producto")
+        console.log("3. Cantidad del producto")
+        console.log("0. Salir")
+        modificacionOpcion = +prompt("Ingrese un numero según la opción a modificar")
+
+    }else{
+        
+        console.log("Numero de producto invalido, por favor vuelva a intentar.")
+
+    }
+
+    
+    switch(modificacionOpcion){
+        case 1:
+            if (indexM != -1){
+                arrayObjetos[indexM].nombre = prompt("Ingrese el nuevo nombre del producto.")
+                console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                break
+            } else {
+                console.log("Numero de producto invalido, por favor vuelva a intentar.")
+            }
+        case 2:
+            if (indexM != -1){
+                arrayObjetos[indexM].valor = prompt("Ingrese el nuevo valor del producto.")
+                console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                break
+            } else {
+                console.log("Numero de producto invalido, por favor vuelva a intentar.")
+            }
+        case 3:
+            if (indexM != -1){
+                arrayObjetos[indexM].cantidad = prompt("Ingrese la nueva cantidad del producto.")
+                console.log("El producto: " + arrayObjetos[indexM].nombre + ", fue modificado con exito")
+                break
+            } else {
+                console.log("Numero de producto invalido, por favor vuelva a intentar.")
+            }
+    }
+    ListaPantalla()
+}
+
+//funcion impime lista en pantalla
+function ListaPantalla() {
+
+    let contenedor = document.getElementById("contenedor")
+    contenedor.innerHTML = ""
+    total = 0
+    for (const producto of arrayObjetos){
+        total = total + ((producto.cantidad) * (producto.valor))
+        const ul = document.createElement("ul")
+        const id = document.createElement("p")
+        const li1= document.createElement("li")
+        const li2= document.createElement("li")
+        const li3= document.createElement("li")
+
+        id.innerText=  `ID: ${producto.idProducto}`
+        li1.innerText= `Producto: ${producto.nombre}`
+        li2.innerText= `Cantidad: ${producto.cantidad}`
+        li3.innerText= `Valor: ${producto.valor}`
+
+        ul.appendChild(li1).appendChild(li2).appendChild(li3)
+        contenedor.appendChild(id).appendChild(ul)
+    }
+    let parrafo = document.createElement("h2")
+    parrafo.innerText = `Monto total de la compra: $${total}`
+    contenedor.append(parrafo)
+}
+
+//funcion click eliminar producto
+function clickEliminarProd() {
+    console.log("Cual producto desea eliminar?")
+    for(const producto of arrayObjetos){
+        console.log(producto.idProducto + ". Producto: " + producto.nombre + ", valor: " + producto.valor + " y cantidad: " + producto.cantidad)
+    }
+    let idE = +prompt("Ingrese numero del producto")
+    let indexE = arrayObjetos.findIndex((producto) => producto.idProducto == idE)
+    if (indexE != -1){
+        let eliminado = arrayObjetos.splice(indexE,1)
+        console.log("El producto: " + eliminado[0].nombre + ", fue eliminado con exito")
+        arrayObjetos.forEach((producto, indice) => {
+            producto.idProducto = indice + 1
+        })
+    } else {
+        console.log("Nombre de producto invalido, por favor vuelva a intentar.")
+    }
+    hidennButonn()
+    ListaPantalla()
+}
+
+//funcion esconder botones cuando no hay nada en la lista
+function hidennButonn(){
+
+    if (arrayObjetos.length != 0) {
+    
+        document.getElementById("btnmodprod").hidden = false
+        document.getElementById("btneliminarprod").hidden = false
+    
+    }else{
+    
+        document.getElementById("btnmodprod").hidden = true
+        document.getElementById("btneliminarprod").hidden = true
+    }
+
+
 }
